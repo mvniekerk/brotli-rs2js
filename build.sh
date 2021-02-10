20aa527e569fda3073cb7f7430040076c6a75d56
Michael van Niekerk <mvniekerk@chazaqdev.com>
2021-02-10 10:31:53 +0200

    Try 1 for publish

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-rm -rf lib
+rm -rf brotli-lib
 docker build -t brotli-rs2js .
-docker run --name brotli-r2js-build brotli-rs2js exit 0
+docker rm brotli-r2js-build
+docker run --name brotli-r2js-build brotli-rs2js echo 0
+mkdir brotli-lib
+docker cp brotli-r2js-build:/pkg brotli-lib
+docker rm brotli-r2js-build
+rm -rf lib
 mkdir lib
-docker cp brotli-r2js-build:/pkg lib/
+cp brotli-lib/pkg/brotli* brotli-lib/pkg/index* lib/
+cp lib/brotli_rs2js.d.ts lib/index.d.ts
