d402bb58d09a42d50417014fa4cef1bd5d349a80
Michael van Niekerk <mvniekerk@chazaqdev.com>
2021-02-10 14:20:58 +0200

    Can't seem to put this damn thing into JS

--- src/lib.rs
+++ src/lib.rs
@@ -12,12 +12,13 @@ use wasm_bindgen::__rt::std::io::{Write, Read};
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-fn brotli_compress(buf: &Vec<u8>) -> Result<Vec<u8>, JsValue> {
+#[no_mangle]
+pub fn brotli_compress(buf: &Vec<u8>) -> Result<Vec<u8>, ()> {
     // (false, buf.clone())
     let compressed_buf = Vec::with_capacity(buf.len());
     let mut w = compressed_buf.writer();
     let mut c = CompressorWriter::new(&mut w, 4096, 11, 22);
-    c.write_all(buf.as_slice()).map_err(|_| JsValue::from_str("Could not write"))?;
+    c.write_all(buf.as_slice()).map_err(|_| ())?;
     Ok(
         c.flush()
             .map(|_| {
@@ -32,22 +33,31 @@ fn brotli_compress(buf: &Vec<u8>) -> Result<Vec<u8>, JsValue> {
     )
 }
 
-
+#[no_mangle]
 pub fn brotli_copy<R: Read, W: Write>(source: R, mut dest: W) -> std::io::Result<u64> {
     let mut decoder = brotli::Decompressor::new(source, 4096);
     std::io::copy(&mut decoder, &mut dest)
 }
 
+#[no_mangle]
+pub fn brotli_decompress(arr: &Vec<u8>) -> Result<Vec<u8>, ()> {
+    let buf = Vec::with_capacity(arr.len());
+    let mut w = buf.writer();
+    brotli_copy(arr.reader(), &mut w).map_err(|_| ())?;
+    let ret = w.get_ref().to_vec();
+    Ok(ret)
+}
+
 #[wasm_bindgen]
-pub fn compress(arr: Uint8Array) -> Result<Uint8Array, JsValue> {
+pub fn compress_js(arr: Uint8Array) -> Result<Uint8Array, JsValue> {
     let arr = arr.to_vec();
-    let ret = brotli_compress(&arr)?;
+    let ret = brotli_compress(&arr).map_err(|_| JsValue::from_str("Could not compress"))?;
     let ret = ret.as_slice();
     Ok(Uint8Array::from(ret))
 }
 
 #[wasm_bindgen]
-pub fn decompress(arr: Uint8Array) -> Result<Uint8Array, JsValue> {
+pub fn decompress_js(arr: Uint8Array) -> Result<Uint8Array, JsValue> {
     use bytes::Buf;
 
     let ret = arr.to_vec();
